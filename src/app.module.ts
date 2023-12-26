d00e14e97ffb5f41220eb9444241e3c0462c9272
trickaugusto <trickaugusto1206@gmail.com>
2023-12-26 14:00:54 -0300

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DiscordBotModule } from './discord-bot/discord-bot.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DiscordBotModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
