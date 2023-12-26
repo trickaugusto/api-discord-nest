0e76c46bf8b869876ee55e04fa98df181e1893d7
trickaugusto <trickaugusto1206@gmail.com>
2023-12-26 14:39:39 -0300

    imports modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DiscordBotModule } from './discord-bot/discord-bot.module';
 import { PaymentModule } from './payment/payment.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, DiscordBotModule, PaymentModule],
+  imports: [UsersModule, DiscordBotModule, PaymentModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
