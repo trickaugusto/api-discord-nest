c8819f01f0778c1d521291cfcf07185c4a9cdcc0
trickaugusto <trickaugusto1206@gmail.com>
2023-12-26 15:54:00 -0300

    create DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  username: string;
+  nickname: string;
+  password: string;
+  email: string;
+  created_at?: Date | string;
+  updated_at?: Date | string;
+}
