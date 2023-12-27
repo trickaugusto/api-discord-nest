c8aa293e8a2ab476c06f2f6a23b3abde59962708
trickaugusto <trickaugusto1206@gmail.com>
2023-12-27 12:05:56 -0300

    add validator in DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,23 @@
+import { IsString, IsNotEmpty, IsEmail } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   nickname: string;
+  
+  @IsString()
+  @IsNotEmpty()
   password: string;
+  
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+  
   created_at?: Date | string;
   updated_at?: Date | string;
 }
