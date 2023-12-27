0b74471d6da9a11fced97bc6aa963fc6b23bb7dd
trickaugusto <trickaugusto1206@gmail.com>
2023-12-27 14:22:47 -0300

    create validator DTO n update main

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsEmail } from "class-validator";
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -8,16 +8,16 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   nickname: string;
-  
+
   @IsString()
   @IsNotEmpty()
   password: string;
-  
+
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
-  
+
   created_at?: Date | string;
   updated_at?: Date | string;
 }
