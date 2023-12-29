35a769dc7a76f225bda147624571e0737294e4d7
trickaugusto <trickaugusto1206@gmail.com>
2023-12-29 00:03:01 -0300

    exports n lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { UsersModule } from './users/users.module';
 import { DiscordBotModule } from './discord-bot/discord-bot.module';
 import { PaymentModule } from './payment/payment.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DiscordBotModule, PaymentModule, PrismaModule],
+  imports: [
+    UsersModule,
+    DiscordBotModule,
+    PaymentModule,
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
