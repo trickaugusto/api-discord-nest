e8ff3e5b9e8743dc46bd5d4deb3af0ee56cc5a70
trickaugusto <trickaugusto1206@gmail.com>
2023-12-28 23:59:59 -0300

    add jwtAuthGuard in routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,11 +8,13 @@ import {
   Delete,
   NotFoundException,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UserEntity } from './entities/user.entity';
+import { UserEntity } from './entity/user.entity';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,12 +26,14 @@ export class UsersController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll() {
     const users = await this.usersService.findAll();
     return users.map((user) => new UserEntity(user));
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const user = await this.usersService.findOne(+id);
 
@@ -41,6 +45,7 @@ export class UsersController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
@@ -55,6 +60,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async remove(@Param('id', ParseIntPipe) id: number) {
     const user = await this.usersService.findOne(+id);
 
