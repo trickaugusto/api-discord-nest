d44dcf667c7e118a390df36861bd3c75ff7ffe06
trickaugusto <trickaugusto1206@gmail.com>
2023-12-30 15:01:47 -0300

    update module

--- src/discord-bot/discord-bot.module.ts
+++ src/discord-bot/discord-bot.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DiscordBotService } from './discord-bot.service';
 import { DiscordBotController } from './discord-bot.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [DiscordBotController],
   providers: [DiscordBotService],
+  imports: [PrismaModule],
 })
 export class DiscordBotModule {}
