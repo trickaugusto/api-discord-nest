42ab801310ef68c372c585f07bfb7fe5906f78b9
trickaugusto <trickaugusto1206@gmail.com>
2023-12-30 15:00:52 -0300

    update entity and dto

--- src/discord-bot/dto/create-discord-bot.dto.ts
+++ src/discord-bot/dto/create-discord-bot.dto.ts
@@ -1,1 +1,15 @@
-export class CreateDiscordBotDto {}
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+
+export class CreateDiscordBotDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  owner_id: number;
+}
--- src/discord-bot/entities/discord-bot.entity.ts
+++ src/discord-bot/entities/discord-bot.entity.ts
@@ -1,1 +1,20 @@
-export class DiscordBot {}
+import { Exclude } from 'class-transformer';
+
+export class DiscordBotEntity {
+  constructor(partial: Partial<DiscordBotEntity>) {
+    Object.assign(this, partial);
+  }
+
+  id: number;
+  name: string;
+  owner_id: number;
+
+  @Exclude()
+  token: string;
+
+  @Exclude()
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+}
