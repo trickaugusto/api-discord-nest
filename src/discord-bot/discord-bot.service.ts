d21d625a9ed12424aa46a6e9f30ecaf8ba7862f7
trickaugusto <trickaugusto1206@gmail.com>
2023-12-30 14:59:40 -0300

    defining service methods and returns

--- src/discord-bot/discord-bot.service.ts
+++ src/discord-bot/discord-bot.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDiscordBotDto } from './dto/create-discord-bot.dto';
 import { UpdateDiscordBotDto } from './dto/update-discord-bot.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class DiscordBotService {
-  create(createDiscordBotDto: CreateDiscordBotDto) {
-    return 'This action adds a new discordBot' + createDiscordBotDto;
+  constructor(private prisma: PrismaService) {}
+
+  async create(createDiscordBotDto: CreateDiscordBotDto) {
+    return this.prisma.bot.create({ data: createDiscordBotDto });
   }
 
-  findAll() {
-    return `This action returns all discordBot`;
+  findAllByOwnerId(owner_id: number) {
+    return this.prisma.bot.findMany({ where: { owner_id } });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} discordBot`;
+    return this.prisma.bot.findUnique({ where: { id } });
   }
 
   update(id: number, updateDiscordBotDto: UpdateDiscordBotDto) {
-    return `This action updates a #${id} discordBot` + updateDiscordBotDto;
+    return this.prisma.bot.update({ where: { id }, data: updateDiscordBotDto });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} discordBot`;
+    return this.prisma.bot.delete({ where: { id } });
   }
 }
